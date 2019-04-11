c691eaf66b25871c63bfe312e1dd1462533495d8
Luke Phyall <shoreditchwarlock@gmail.com>
2019-04-11 12:16:35 +0100

    Adds simplecov and rubocop. Rubocop might have been a mistake.

--- Gemfile
+++ Gemfile
@@ -4,4 +4,6 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-# gem "rails"
+gem 'simplecov', require: false
+gem 'simplecov-console', require: false
+gem 'rubocop', '0.56.0'
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -5,11 +5,10 @@ describe Statement do
   let (:state) { Statement.new }
   let (:trans) { double :transactions, read: [{date: "10/04/2019", balance: "100.00", amount: "50.00 || -"}] }
 
-
+  context "#format" do
     it "Formats all your transaction data nicely" do
       expect { state.format(trans.read) }.to output("date || credit || debit || balance\n10/04/2019 || 50.00 || - || 100.00\n").to_stdout
-
     end
-
+  end
 
 end
